36fbbf67988bc1d64194aa473ad58085b8aa6f80
JKCoffee <almind@Almindui-MacBook-Pro.local>
2013-07-07 19:04:57 +0900

    test

--- TestApp/TestApp/JKDocument.m
+++ TestApp/TestApp/JKDocument.m
@@ -14,16 +14,13 @@
 {
     self = [super init];
     if (self) {
-        // Add your subclass-specific initialization here.
-    }
+   }
     return self;
 }
 
 - (NSString *)windowNibName
 {
-    // Override returning the nib file name of the document
-    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
-    return @"JKDocument";
+    return @"Test";
 }
 
 - (void)windowControllerDidLoadNib:(NSWindowController *)aController
