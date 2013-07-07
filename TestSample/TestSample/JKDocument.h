e9f0cabd0bc2438b4d71e861e45ffbe7b686ab36
Almind <almind@Almindui-MacBook-Pro.local>
2013-07-07 18:42:18 +0900

    sample commit

---
+++ TestSample/TestSample/JKDocument.h
@@ -0,0 +1,13 @@
+//
+//  JKDocument.h
+//  TestSample
+//
+//  Created by Almind on 7/7/13.
+//  Copyright (c) 2013 AlmindTest. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JKDocument : NSDocument
+
+@end
